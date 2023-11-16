b7e588a4297fc794044c19674228c795c3fb692d
BinhTran68 <binhtd2268@gmail.com>
2023-11-16 23:26:35 +0700

    update user model

--- DuAnAgileNhom1/src/model/User.java
+++ DuAnAgileNhom1/src/model/User.java
@@ -17,5 +17,64 @@ public class User {
     private String password;
     
     private String fullName;
+        
+    private String gioiTinh;
+
+    public User(String id, String username, String password, String fullName, String gioiTinh) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+        this.gioiTinh = gioiTinh;
+    }
+
+    public User() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(String gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", username=" + username + ", password=" + password + ", fullName=" + fullName + ", gioiTinh=" + gioiTinh + '}';
+    }
+    
     
 }
